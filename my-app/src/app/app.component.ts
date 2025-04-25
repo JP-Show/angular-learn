957ec0524a73a430ee4b82e69c68f7cafab3b55b
Drezzy <andreluiz.jpofi@gmail.com>
2025-04-25 12:47:45 -0300

    Learn about components

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-app';
+  newProperties:string = "Hello World"
+
+
 }
---
+++ my-app/src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    // template:"<h1>Profile Component<h1>",
+    templateUrl: "./profile.component.html",
+    styles: "h1{color:red}"
+})
+export class ProfileComponent{
+
+}
